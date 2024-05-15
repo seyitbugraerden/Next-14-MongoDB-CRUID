c4a5d626f51e3fc83f2ab34e3289acb864757017
seyitbugraerden <seyitbugraerden@gmail.com>
2024-05-15 22:11:20 +0300

    prisma update

--- utils/prisma.js
+++ utils/prisma.js
@@ -2,7 +2,3 @@ import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient()
 
-const client = globalThis.prisma || new PrismaClient()
-if (process.env.DATABASE_URL !== "production") globalThis.prisma = client
-
-export default client
